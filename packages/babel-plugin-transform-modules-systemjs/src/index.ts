7e3eb2dfde56ab30a76331d31cb610588cb4c979
Bogdan Savluk <savluk.bogdan@gmail.com>
2020-04-19 03:30:21 +0200

    babel-plugin-proposal-numeric-separator

--- packages/babel-plugin-transform-modules-systemjs/src/index.ts
+++ packages/babel-plugin-transform-modules-systemjs/src/index.ts
@@ -31,7 +31,7 @@ WARNING: Dynamic import() transformation must be enabled using the
 //todo: use getExportSpecifierName in `helper-module-transforms` when this library is refactored to NodePath usage.
 
 export function getExportSpecifierName(
-  node: Node,
+  node: t.Node,
   stringSpecifiers: Set<string>,
 ): string {
   if (node.type === "Identifier") {
@@ -550,6 +550,7 @@ export default declare((api, options) => {
           });
 
           let moduleName = getModuleName(this.file.opts, options);
+          // @ts-expect-error todo(flow->ts): do not reuse variables
           if (moduleName) moduleName = t.stringLiteral(moduleName);
 
           hoistVariables(
